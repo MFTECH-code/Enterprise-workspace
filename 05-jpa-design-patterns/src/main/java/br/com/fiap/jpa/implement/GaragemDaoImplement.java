597fadc8e8a4cc7534d1fa092769c37318f4fca0
Logon Aluno <logonrmlocal@fiap.com.br>
2022-03-31 22:51:07 -0300

    Exercicio

--- 05-jpa-design-patterns/src/main/java/br/com/fiap/jpa/implement/GaragemDaoImplement.java
+++ 05-jpa-design-patterns/src/main/java/br/com/fiap/jpa/implement/GaragemDaoImplement.java
@@ -2,9 +2,10 @@ package br.com.fiap.jpa.implement;
 
 import javax.persistence.EntityManager;
 
+import br.com.fiap.jpa.dao.GaragemDao;
 import br.com.fiap.jpa.entities.Garagem;
 
-public class GaragemDaoImplement extends DaoImplement<Garagem, Integer> {
+public class GaragemDaoImplement extends DaoImplement<Garagem, Integer> implements GaragemDao {
 
 	public GaragemDaoImplement(EntityManager em) {
 		super(em);
--- 05-jpa-design-patterns/src/main/java/br/com/fiap/jpa/main/Exemplo.java
+++ 05-jpa-design-patterns/src/main/java/br/com/fiap/jpa/main/Exemplo.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManagerFactory;
 import br.com.fiap.jpa.entities.Garagem;
 import br.com.fiap.jpa.entities.Tipo;
 import br.com.fiap.jpa.entities.Veiculo;
+import br.com.fiap.jpa.exception.InexistentItemException;
 import br.com.fiap.jpa.implement.GaragemDaoImplement;
 import br.com.fiap.jpa.implement.VeiculoDaoImplement;
 import br.com.fiap.jpa.singleton.EntityManagerFactorySingleton;
@@ -17,16 +18,43 @@ public class Exemplo {
 	public static void main(String[] args) {
 		EntityManagerFactory factory = EntityManagerFactorySingleton.getInstance();
 		EntityManager em = factory.createEntityManager();
-		
-		Garagem garagem = new Garagem("Garagem dois amigos");
-		Veiculo veiculo = new Veiculo("SUV", 200000.0, 2010, LocalDate.now(), Tipo.CARRO, "123FACA");
-		
-		
 		GaragemDaoImplement garagemDao = new GaragemDaoImplement(em);
-		garagemDao.insert(garagem);
-		
 		VeiculoDaoImplement veiculoDao = new VeiculoDaoImplement(em);
+		
+		Garagem garagem = new Garagem("Garagem quatro amigos");
+		Veiculo veiculo = new Veiculo("SUV", 200000.0, 2010, LocalDate.now(), Tipo.CARRO, "123A");
+		
+		garagemDao.insert(garagem);
 		veiculoDao.cadastrar(veiculo);
+		
+		Garagem garagem2 = new Garagem("Garagem tres amigos");
+		Veiculo veiculo2 = new Veiculo("SUV", 200000.0, 2010, LocalDate.now(), Tipo.CARRO, "12asCA");
+		
+		garagemDao.insert(garagem);
+		veiculoDao.cadastrar(veiculo);
+		
+		try {
+			Garagem garagem3 = garagemDao.find(1);
+			Veiculo veiculo3 = veiculoDao.procurar(1);
+			garagem3.setNome("KKKKKKKKKKKK");
+			veiculo3.setValor(99999.9);
+			
+			garagemDao.update(garagem3);
+			veiculoDao.atualizar(veiculo3);
+		} catch (InexistentItemException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		try {
+			garagemDao.remove(1);
+			veiculoDao.apagar(1);
+		} catch (InexistentItemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 		factory.close();
 	}
